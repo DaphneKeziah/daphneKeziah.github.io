9ac29efd050cf9076d8b27e4544893d5ea0287c6
Daphne Keziah <dkeziah@broadsoft.com>
2017-07-25 17:15:31 +0530

    Initial commit

---
+++ assets/js/toDo.js
@@ -0,0 +1,19 @@
+//Check off specific to do by clicking
+
+$("ul").on("click","li",function () {
+	$(this).toggleClass("completed");
+});
+
+$("ul").on("click","span",function(event){
+	$(this).parent().fadeOut(500,function(){
+	$(this).remove();
+});
+	event.stopPropagation();
+});
+$("input[type='text']").keypress(function(event) {
+ 	if(event.which === 13){
+ 		var toDoText = $(this).val();
+ 		$(this).val("");
+ 		$("ul").append("<li><span><i class='fa fa-trash'></i></span> "+toDoText+"</li>");
+ 	}
+});
