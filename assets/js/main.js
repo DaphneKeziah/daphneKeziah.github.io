331247963fca1966e6df05bc9ca291c0211faee0
Daphne Keziah <dkeziah@broadsoft.com>
2017-08-03 21:39:38 +0530

    Changed http

--- assets/js/main.js
+++ assets/js/main.js
@@ -203,30 +203,30 @@
           }
 
           //everything looks good! proceed...
-          if(proceed) 
-          {
-              //data to be sent to server
-              post_data = {'userName':user_name, 'userEmail':user_email, 'userPhone':user_phone, 'userMessage':user_message};
-              //Ajax post data to server
-              $.post('contact_me.php', post_data, function(response){  
+          // if(proceed) 
+          // {
+          //     //data to be sent to server
+          //     post_data = {'userName':user_name, 'userEmail':user_email, 'userPhone':user_phone, 'userMessage':user_message};
+          //     //Ajax post data to server
+          //     $.post('contact_me.php', post_data, function(response){  
                   
-                  //load json data from server and output message     
-                  if(response.type == 'error')
-                  {
-                      output = '<div class="error">'+response.text+'</div>';
-                  }else{
+          //         //load json data from server and output message     
+          //         if(response.type == 'error')
+          //         {
+          //             output = '<div class="error">'+response.text+'</div>';
+          //         }else{
                   
-                      output = '<div class="success">'+response.text+'</div>';
+          //             output = '<div class="success">'+response.text+'</div>';
                       
-                      //reset values in all input fields
-                      $('#contact_form input').val(''); 
-                      $('#contact_form textarea').val(''); 
-                  }
+          //             //reset values in all input fields
+          //             $('#contact_form input').val(''); 
+          //             $('#contact_form textarea').val(''); 
+          //         }
                   
-                  $("#result").hide().html(output).slideDown();
-              }, 'json');
+          //         $("#result").hide().html(output).slideDown();
+          //     }, 'json');
               
-          }
+          // }
       });
       
       //reset previously set border colors and hide all message on .keyup()
